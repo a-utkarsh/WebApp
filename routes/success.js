96ef3641335e0d90c5a20cbaec468c9537ebca77
Utkarsh Anand <utkarsh@utkarsh-Lenovo-ideapad-500-15ISK>
2019-06-14 18:21:56 +0530

    app

--- routes/success.js
+++ routes/success.js
@@ -5,6 +5,7 @@ var signup	= require('./signup');
 router.get('/',on_success);
 
 function on_success(req, res,next){
+	console.log(req);
 	console.log("show success page");
 	res.render("success.html");
 }
